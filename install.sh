fecab95b849bfa10077fbdb4d9c1227111a598f8
benterris <benjamin.terris@gmail.com>
2018-12-15 02:47:01 +0100

    change vim colors + add ln manb

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# File locations may vary, these may not all work
+
 ln -sf "/home/$SUDO_USER/.custom-config/vscode/settings.json" "/home/$SUDO_USER/.config/Code/User/settings.json"
 ln -sf "/home/$SUDO_USER/.custom-config/vscode/keybindings.json" "/home/$SUDO_USER/.config/Code/User/keybindings.json"
 ln -sf "/home/$SUDO_USER/.custom-config/.zshrc" "/home/$SUDO_USER/.zshrc"
 ln -sf "/home/$SUDO_USER/.custom-config/rebellion.json" "/home/$SUDO_USER/.vscode/extensions/daylerees.rainglow-1.5.2/themes/rebellion.json"
 ln -sf "/home/$SUDO_USER/.custom-config/us" "/usr/share/X11/xkb/symbols/us"
 ln -sf "/home/$SUDO_USER/.custom-config/terminator-config" "/home/$SUDO_USER/.config/terminator/config"
-ln -sf "/home/$SUDO_USER/.custom-config/vimrc" "/home/$SUDO_USER/.vim/vimrc"
+ln -sf "/home/$SUDO_USER/.custom-config/vim/vimrc" "/home/$SUDO_USER/.vim/vimrc"
 ln -sf "/home/$SUDO_USER/.custom-config/.gitconfig" "/home/$SUDO_USER/.gitconfig"
+ln -sf "/home/$SUDO_USER/.custom-config/vim/mydelek.vim" "/usr/share/vim/vim80/colors/mydelek.vim"
